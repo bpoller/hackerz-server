e3275402747154b0d427d02aca671c1a7ef6db5b
Bert Poller <bpoller@ekito.fr>
2014-03-22 11:39:59 +0100

    Integration with Kinvey

--- app/controllers/Sigfox.java
+++ app/controllers/Sigfox.java
@@ -1,22 +1,72 @@
 package controllers;
 
-import play.Logger.ALogger;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.xml.bind.DatatypeConverter;
+
 import play.mvc.Controller;
-import play.mvc.Http.RequestBody;
 import play.mvc.Result;
 
 public class Sigfox extends Controller {
 
+	/**
+	 * Kinvey App key
+	 */
+	static final String APP_KEY = System.getenv("APP_KEY");
+
+	/**
+	 * Kinvey App key
+	 */
+	static final String MASTER_KEY = System.getenv("MASTER_KEY");
+
+	/**
+	 * URL du datastore Kinvey
+	 */
+	static final String dataStoreURL = "https://baas.kinvey.com/appdata/" + APP_KEY + "/hackerz/";
+
 	public static Result endPoint() {
+		Map<String, String[]> form = request().body().asFormUrlEncoded();
 
-		ALogger logger = play.Logger.of(Sigfox.class);
+		String latitude = form.get("slot.latitude")[0];
+		String longitude = form.get("slot.longitude")[0];
 
-		RequestBody body = request().body();
+		// Données à passer en POST, au format JSON
+		StringBuffer data = new StringBuffer();
+		data.append("{\"latitude\":\"" + latitude + "\"}");
+		data.append("{\"longitude\":\"" + longitude + "\"}");
+		data.append("{\"timestamp\":\"" + System.currentTimeMillis() + "\"}");
 
-		logger.error("lat: " + body.asFormUrlEncoded().get("slot.latitude")[0]);
+		try {
+			send(connection(), data.toString());
+		} catch (IOException ioe) {
+			return status(INTERNAL_SERVER_ERROR);
+		}
 
-		logger.error("long: " + body.asFormUrlEncoded().get("slot.longitude")[0]);
+		return ok();
+	}
 
-		return ok("OK.");
+	private static HttpsURLConnection connection() throws IOException {
+
+		URL url = new URL(dataStoreURL);
+		HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+		con.setRequestMethod("POST");
+		con.setRequestProperty("Content-Type", "application/json");
+
+		String auth = new String(APP_KEY + ":" + MASTER_KEY);
+		String encodedAuth = DatatypeConverter.printBase64Binary(auth.getBytes("UTF-8"));
+		con.setRequestProperty("Authorization", "Basic " + encodedAuth);
+		return con;
+	}
+
+	private static void send(HttpsURLConnection connection, String data) throws IOException {
+		connection.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+		wr.writeBytes(data);
+		wr.flush();
+		wr.close();
 	}
 }
