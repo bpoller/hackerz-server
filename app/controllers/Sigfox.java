b5a825c534ea33758daf3e7ddae8e629b4a1e89d
Bert Poller <bpoller@ekito.fr>
2014-03-22 10:38:15 +0100

    test

---
+++ app/controllers/Sigfox.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import play.Logger.ALogger;
+import play.mvc.Controller;
+import play.mvc.Http.RequestBody;
+import play.mvc.Result;
+
+public class Sigfox extends Controller {
+
+	public static Result endPoint() {
+
+		ALogger logger = play.Logger.of(Sigfox.class);
+
+		RequestBody body = request().body();
+		
+		logger.error(body.toString());
+
+		return ok("OK.");
+	}
+}
