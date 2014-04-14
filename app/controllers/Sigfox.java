0f2a9c79ed5b959342d12de2ac2b30b1b961e4c8
Bert Poller <bpoller@ekito.fr>
2014-04-14 14:18:52 +0200

    tried to deploy to heroku. too slow. fail.

--- app/controllers/Sigfox.java
+++ app/controllers/Sigfox.java
@@ -1,8 +1,5 @@
 package controllers;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import play.libs.Json;
 import play.libs.WS;
 import play.libs.WS.WSRequestHolder;
@@ -14,7 +11,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Sigfox extends Controller {
 
-		/**
+	/**
 	 * Kinvey App key
 	 */
 	static final String APP_KEY = System.getenv("APP_KEY");
@@ -29,14 +26,14 @@ public class Sigfox extends Controller {
 	 */
 	static final String dataStoreURL = "https://baas.kinvey.com/appdata/" + APP_KEY + "/trackEDF/";
 
-	public static Result saveData(String data) {
+	public static Result save(String time, String data) {
 		ObjectNode json = Json.newObject();
 		json.put("tariff", extractTariff(data));
 		json.put("transitionIndex", extractTransitionIndex(data));
-		json.put("date", new SimpleDateFormat().format(new Date()));
+		json.put("time", time);
 		ArrayNode values = json.putArray("values");
-		copyInto(values,data);
-		
+		copyInto(values, data);
+
 		WSRequestHolder request = WS.url(dataStoreURL);
 		request.setAuth(APP_KEY, MASTER_KEY);
 		request.post(json);
@@ -44,17 +41,17 @@ public class Sigfox extends Controller {
 	}
 
 	private static void copyInto(ArrayNode values, String data) {
-		for(int i = 4; i < 24; i=i+2){
-			String sVal = data.substring(i,i+2);
-			values.add(Integer.parseInt(sVal,16));
+		for (int i = 4; i < 24; i = i + 2) {
+			String sVal = data.substring(i, i + 2);
+			values.add(Integer.parseInt(sVal, 16));
 		}
 	}
 
 	private static int extractTransitionIndex(String data) {
-		return Integer.parseInt(data.substring(0,2));
+		return Integer.parseInt(data.substring(0, 2));
 	}
 
 	private static String extractTariff(String data) {
-		return data.substring(2, 4).equals("01")? "PEAK": "OFF_PEAK";
-	}	
+		return data.substring(2, 4).equals("01") ? "PEAK" : "OFF_PEAK";
+	}
 }
