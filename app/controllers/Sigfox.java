40b2cca18cbf0be541657b73f2c9ac80761f4b34
Bert Poller <bpoller@ekito.fr>
2014-04-15 23:37:47 +0200

    setup the first highchart

--- app/controllers/Sigfox.java
+++ app/controllers/Sigfox.java
@@ -1,9 +1,12 @@
 package controllers;
 
-import play.libs.F.Promise;
-import play.libs.Json;
-import play.libs.WS;
+import static play.libs.Json.newObject;
+
+import java.util.Date;
+
 import play.libs.F.Function;
+import play.libs.F.Promise;
+import play.libs.WS;
 import play.libs.WS.WSRequestHolder;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -12,6 +15,8 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Sigfox extends Controller {
+
+	private static final int ONE_MINUTE = 60000;
 
 	/**
 	 * Kinvey App key
@@ -29,7 +34,7 @@ public class Sigfox extends Controller {
 	static final String dataStoreURL = "https://baas.kinvey.com/appdata/" + APP_KEY + "/trackEDF/";
 
 	public static Promise<Result> save(String time, String data) {
-		ObjectNode json = Json.newObject();
+		ObjectNode json = newObject();
 		json.put("tariff", extractTariff(data));
 		json.put("transitionIndex", extractTransitionIndex(data));
 		json.put("time", time + "000");
@@ -43,6 +48,36 @@ public class Sigfox extends Controller {
 				return status(response.getStatus(), response.getBody());
 			}
 		});
+	}
+
+	public static Result chart(long start, long end, String callback) {
+		String resultString = callback + "(" + getData(start, end).toString() + ");";
+		return ok(resultString);
+	}
+
+	private static ArrayNode getData(long start, long end) {
+		ArrayNode dataNode = newObject().arrayNode();
+	
+		 System.out.println("Start " + new Date(start));
+		 System.out.println("End " + new Date(end));
+		 long interval = calculateInterval(start, end);
+		 System.out.println(interval);
+		 
+		for (long i = start; i < end; i += interval) {
+			ArrayNode line = dataNode.addArray();
+			line.add(i );
+			line.add(Math.random() * 100);
+		}
+
+		return dataNode;
+	}
+
+	private static long calculateInterval(long start, long end) {
+		long step = (end - start) / 144;
+		if (step < ONE_MINUTE) {
+			step = ONE_MINUTE;
+		}
+		return step;
 	}
 
 	private static void copyInto(ArrayNode values, String data) {
